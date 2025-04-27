03890e3ef6a7ba898725157c38612eb08608f3b1
ankitojha07 <ankitojha1409@gmail.com>
2025-04-28 00:25:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = reqiure("http");
+const os = reqiure("os");
+const process = reqiure("process");
+const url = reqiure("url");
+
+//! Format bytes to human-readable format
+//? Format seconds to human redable time
+// get CPU info
+// get memory info
+// get os info
+// get User info
+// get network info
+// get process info
+//! Get HTTP info
